5aa8efc85c97d518e9d065e866ca8b52fa81e45b
Tolben <vitek.pasek@gmail.com>
2025-10-22 02:18:52 +0200

    chore(schema): add/update generated schema + README (umbrella sync)

--- schema/030_foreign_keys.sql
+++ schema/030_foreign_keys.sql
@@ -1,3 +1,3 @@
--- Auto-generated from schema-map.psd1 @ 1e83bb6 (2025-10-21T10:18:36+02:00)
+-- Auto-generated from schema-map.psd1 (map@1e83bb6)
 -- table: key_rotation_jobs
 ALTER TABLE key_rotation_jobs ADD CONSTRAINT fk_key_rotation_jobs_user FOREIGN KEY (executed_by) REFERENCES users(id) ON DELETE SET NULL;
